f79c802a779e77af74d4a6f2c0d1e478fac80008
mario.hh <mario.hh@kakaoent.com>
2023-12-06 16:17:31 +0900

    add setting

---
+++ mario.sh
@@ -0,0 +1,25 @@
+CUDA_VISIBLE_DEVICES=0 python train.py \
+--dataroot IAM/train \
+--ttfRoot data_iam/fonts_iam \
+--corpusRoot data_iam/seen_char.txt \
+--alphabet data_iam/seen_char.txt \
+--dictionaryRoot data_iam/iam_dictionary.txt \
+--name exp \
+--model character \
+--no_dropout \
+--batch_size 16 \
+--imgH 128 \
+--imgW 128 \
+--num_writer 399 \
+--num_writer_emb 256 \
+--gpu_ids 0 \
+--lr 0.0001 \
+--lr_decay_iters 30 \
+--niter_decay 30 \
+--G_ch 64 \
+--D_ch 16 \
+--max_length 64 \
+--hidden_size 256 \
+--val_num 10 \
+--val_seenstyleRoot IAM/seenstyleRoot.txt \
+--val_unseenstyleRoot IAM/seenstyleRoot.txt
